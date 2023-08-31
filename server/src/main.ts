3e0fd618055ced3b5f7b09b80320bfff5139b1ad
Va <phi.va96@gmail.com>
2023-08-31 00:10:21 -0400

    updated main server file

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
+    allowedHeaders: ['content-type'],
     origin: [
       'http://localhost:3000',
       'http://www.varecipes.xyz'
